155890c79ce0e067186b665de18b0070925f87ee
unknown <akshaysr_bt2k16@dtu.ac.in>
2020-07-13 00:42:02 +0530

    new file

---
+++ Project.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#this will be used for capstone project
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[ ]:
+
+
+
+
